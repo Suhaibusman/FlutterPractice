feb0fef3cff5389100fd4b96092cac46cd14acbf
Suhaibusman <suhaibusman54@gmail.com>
2023-06-22 10:52:21 -0700

    list

--- class4.dart
+++ class4.dart
@@ -15,4 +15,44 @@ if (number % 5 == 0 || number % 7 ==0) {
   print("number is not divisible by 5 or 7");
 }
 
+var email ="suhaibusman54@gmail.com";
+var password =12345;
+if(email =="suhaibusman54@gmail.com" && password == 12345){
+  print("email and password is match");
+}else{
+  print("email and password is not matched");
+}
+
+var list1 =["suhaib usman",20,"muslim","Pakistani"];
+print(list1);
+print(list1.length);
+print(list1.first);
+
+list1.add(12);
+print(list1);
+
+var list2 =["shakeeb", 900, 80,true,false];
+print(list2);
+// var data=list1.addAll(list1);
+// print(list1.addAll);
+
+List<int> id =[1,2,4,5,6];
+print(id.reversed);
+
+print(id);
+// id[2]=44;/
+
+print(id[id.length-2]);
+
+var a=list1.addAll(list2);
+print(list1);
+
+list1.replaceRange(list1.length-1,list1.length,["uuuuuu",90,788]);
+print(list1);
+// list1.clear();
+print(list1);
+
+
+
+
 }
--- class5.dart
+++ class5.dart
@@ -1,9 +1,71 @@
-import 'dart:io';
+
 
 void main() {
-// int numq = int.parse(stdin.readLineSync()!);
-// int number = numq+1;
-// print(number);
+var studentname =["suhaib", "usman","shakeeb"];
+List name =["sks",'ssd','ssdf','sfr'];
+name.insert(2,"check");
+print(name);
 
 
+Map nameofstu ={
+
+  'id' : [122,123],
+  'name' : ["suhaib","shakeeb"],
+  "age" : [20,15]
+};
+// print(nameofstu.containsKey("id"));
+
+// print(nameofstu["name"][1]);
+// print(nameofstu["id"][1]);
+// print(nameofstu["age"][1]);
+
+List data1 =[
+{"name" :"suhaib","rollno":122},
+{"name" :"shakeeb",9:123}];
+// print(data1[0]);
+
+Map <String, dynamic> studentdata1= {
+  "name": "suhaib",
+  "age" :20,
+  "semester":6
+};
+//add on map
+studentdata1["nationality"]="Pakistani";
+print(studentdata1);
+// putIfAbsent agr woh  key add nhi he to dosri add add krdoo
+// other wise chordoo
+
+studentdata1.putIfAbsent("name", () => "amash"); // abhi yeh ad nhi hoga q ke already name ki key hee
+print(studentdata1);
+studentdata1.putIfAbsent("marks", () => 99); // ab yeh add krdega q ke mark name ki key available nhi hee
+print(studentdata1);
+
+
+//remove key 
+
+studentdata1.remove("marks"); //mtlb marks ki key or value dono remove hojaengi
+// print(studentdata1);  // ab marks show nhi honge
+
+// studentdata1.removeWhere((key, value) => false)  yeh check krna hee
+
+List dataa = [
+
+  {0,1},
+  {
+    1:{
+      "hehe":
+      [
+        {
+          "hehe": {
+            "haha":"bye"
+          }
+        }
+      ]
+    }
+  }
+];
+
+print(dataa[1][1]["hehe"][0]["hehe"]["haha"]); //bye output ke lye
+// // print(data1[0]["name"]);
+// print(data1[1][9]);
 }
