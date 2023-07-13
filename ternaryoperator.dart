8548d648c4a91f481dcfa087b814778e94f81c81
Suhaibusman <suhaibusman54@gmail.com>
2023-07-13 10:47:17 -0700

    practice

---
+++ ternaryoperator.dart
@@ -0,0 +1,7 @@
+void main(){
+int age =18;
+ //agr condition true hogi to ? ke bd wali execute hogi or agr false hogi to : kr bd wali hogi
+String allowedlist = age >= 18 ? "Allowed" : "Not Allowed";
+print(allowedlist);
+
+}
