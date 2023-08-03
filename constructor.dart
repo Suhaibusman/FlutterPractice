6288b62d2214c2ec15c778265a86b528a35d4b87
Suhaibusman <suhaibusman54@gmail.com>
2023-08-03 09:44:20 -0700

    class

--- constructor.dart
+++ constructor.dart
@@ -1,4 +1,30 @@
 //jab hum class call krte he to usme koi parameter pass krenge to constructor kehlaegaa
 
 //factory constructor or parameterizied constructor
+//constructor ki different types hoti
 
+//default
+//parameterized
+//named use for kind of method // 
+//factory 
+//rnd constructor
+
+
+
+
+void main(){
+person obj =person("suhaib", 21);
+obj.displayinfo();
+
+}
+
+class person{
+  String? name;
+  int? age;
+  person(this.name,this.age);
+  //  person(name,age);// null ajaega q ke this nhi laga hua this object ka refrence hota hee
+  
+displayinfo(){
+  print("Name is $name and Age is ${age}");
+}
+}
